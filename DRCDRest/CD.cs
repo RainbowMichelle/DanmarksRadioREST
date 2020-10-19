642d7b1caecf6601e4f4118e7576555699a271b2
Michelle Kristensen <mich5h03@edu.easj.dk>
2020-10-19 11:28:57 +0200

    Add project files.

---
+++ DRCDRest/CD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DRCDRest
+{
+    public class CD
+    {
+        private int _ID;
+        private string _title;
+        private string _artist;
+        private double _duration;
+        private int _NumberOfTracks;
+        private int _yearOfPublication;
+        private List<CD> _cder;
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+
+    }
+}
