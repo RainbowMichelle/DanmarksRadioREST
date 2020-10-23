8bbe7e1df232a0c4692902101e4d028c5673a72e
Morten Jakobsen Møller <mort748a@edu.easj.dk>
2020-10-23 11:39:19 +0200

    Navne på artister

--- DRCDRest/Controllers/CDsController.cs
+++ DRCDRest/Controllers/CDsController.cs
@@ -17,7 +17,7 @@ namespace DRCDRest.Controllers
             new CD(1,"xx","jon",10.0, 5, 2000),
             new CD(2,"xy","Chris",10.5, 7, 2032),
             new CD(3,"xxx","Eva",13.0, 2, 2120),
-            new CD(4,"xxxx","Bellion",8.0, 1, 2003),
+            new CD(4,"xxxx","jon",8.0, 1, 2003),
             new CD(5,"xxxxx","Urf",17.0, 6, 2043),
         };
 
